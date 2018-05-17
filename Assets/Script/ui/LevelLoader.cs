f6af430dbfbccbae20a5bf3c65e4b1cfcdc46781
VitalyZ <VitalyZ@users.noreply.github.com>
2018-05-17 21:31:37 +0300

    Levels/exits (wip)

---
+++ Assets/Script/ui/LevelLoader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	public Progressbar progressbar;
+	private AsyncOperation operation;
+
+	public AsyncOperation load(string levelName) {
+		operation = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(levelName);
+		return operation;
+	}
+
+	private void Awake() {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	private void Update() {
+		if(operation != null) {
+			progressbar.value = operation.progress;
+			if (operation.isDone) {
+				Destroy(gameObject);
+			}
+		}
+	}
+}
---
+++ Assets/Script/util/Exit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour {
+
+	public string level;
+	public Transform canvas;
+	public GameObject levelLoader;
+
+	private void OnTriggerEnter(Collider other) {
+		GameObject o = other.gameObject;
+		if (other.tag == "Player") {
+			Instantiate(levelLoader, canvas);
+			LevelLoader loader = levelLoader.GetComponent<LevelLoader>();
+			loader.load(level);
+		}
+	}
+}
