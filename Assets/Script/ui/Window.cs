05d454a190ea4724b6ff1242508065200530417e
VitalyZ <VitalyZ@users.noreply.github.com>
2018-05-26 19:35:47 +0300

    Basic Windows

---
+++ Assets/Script/ui/Window.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Window : MonoBehaviour {
+	protected WindowsManager manager;
+	protected string id;
+
+	virtual public void show(string id, WindowsManager manager, object windowData = null) {
+		this.id = id;
+		this.manager = manager;
+	}
+
+	virtual public void hide() {
+		manager = null;
+	}
+
+	public void close() {
+		manager.close(id);
+	}
+}
---
+++ Assets/Script/ui/WindowsManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WindowsManager : MonoBehaviour {
+
+	public string startWindow;
+	private Dictionary<string, GameObject> windows = new Dictionary<string, GameObject>();
+
+	WindowsManager() {
+
+	}
+
+	private void Start() {
+		if(startWindow != "") {
+			open(startWindow);
+		}
+	}
+
+	public Window open(string windowName, object windowData = null) {
+		if (windows.ContainsKey(windowName)) {
+			return windows[windowName].GetComponent<Window>();
+		}
+		GameObject prefab = Resources.Load<GameObject>("windows/" + windowName);
+		GameObject obj = Instantiate<GameObject>(prefab, gameObject.transform);
+		windows[windowName] = obj;
+		Window window = obj.GetComponent<Window>();
+		window.show(windowName, this, windowData);
+	//	Debug.Log("open " + windowName + obj + window);
+		return window;
+	}
+
+	public Window close(string windowName) {
+		if (!windows.ContainsKey(windowName)) {
+			return null;
+		}
+		Window window = windows[windowName].GetComponent<Window>();
+		window.hide();
+		windows.Remove(windowName);
+		return window;
+	}
+}
